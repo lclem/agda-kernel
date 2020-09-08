416b453416e0d66da4552979550d3785c5575c00
Lorenzo C <clementelorenzo@gmail.com>
2020-09-08 09:03:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='agda_kernel',
-    version='0.62',
+    version='0.63',
     #packages=['src/agda_kernel'],
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
